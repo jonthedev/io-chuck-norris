90cd66646741072c813d95198b8f2f0f25ef8b00
jonathan DK <jonthedev0@gmail.com>
2022-03-04 11:31:45 +0100

    - add star from react icons module into JokeListItem component. - JokeListItem button changes to Add/Remove based on isFavourite state. - add conditional style rendering for star based on isFavourite state. - create addToFavouriteHandler.

--- src/components/JokeListItem/JokesListItem.js
+++ src/components/JokeListItem/JokesListItem.js
@@ -1,12 +1,26 @@
+import { useState } from 'react'
+import { FaStar } from 'react-icons/fa'
 import './JokesListItem.css'
 
 const JokesListItem = ({ joke }) => {
+  const [isFavourite, setIsFavourite] = useState(false)
+
+  const addToFavouriteHandler = () => {
+    setIsFavourite(!isFavourite)
+  }
+
   return (
     <li className='jokes-list-item'>
       <p>{joke.joke}</p>
       <div className='jokes-list-item-action'>
-        <button className='btn'>Add to favourites</button>
-        <span>star</span>
+        <button className='btn' onClick={addToFavouriteHandler}>
+          {isFavourite
+            ? 'Remove from favourites.'
+            : 'Add to favourites'}
+        </button>
+        <span>
+          <FaStar style={isFavourite && { color: 'gold' }} />
+        </span>
       </div>
     </li>
   )
