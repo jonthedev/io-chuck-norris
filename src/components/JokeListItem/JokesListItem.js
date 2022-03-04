2b0e41faabef78cbedd819cde362527bc8ade02c
jonathan DK <jonthedev0@gmail.com>
2022-03-04 11:05:59 +0100

    - create add to favourites button & star holder in JokesListItem template. - create global btn style.

--- src/components/JokeListItem/JokesListItem.js
+++ src/components/JokeListItem/JokesListItem.js
@@ -4,6 +4,10 @@ const JokesListItem = ({ joke }) => {
   return (
     <li className='jokes-list-item'>
       <p>{joke.joke}</p>
+      <div className='jokes-list-item-action'>
+        <button className='btn'>Add to favourites</button>
+        <span>star</span>
+      </div>
     </li>
   )
 }
